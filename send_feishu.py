179b94364f9d078ad59df043bd4d510fe6caa0ad
zhengcls <test@example.com>
2026-04-10 21:33:04 +0800

    feat: 新增睡前故事《小刺猬嘟嘟的月光灯笼》

--- send_feishu.py
+++ send_feishu.py
@@ -1,8 +1,8 @@
 import urllib.request, json
 
 webhook = "https://open.feishu.cn/open-apis/bot/v2/hook/f7b32472-16fb-40c1-8860-fec21f13db66"
-story_title = "小螃蟹圆圆的珍珠海螺"
-story_desc = "在蔚蓝海岸的浅滩上，爱捡贝壳的小螃蟹圆圆捡到了一个会发光会唱歌的神奇海螺，他把温暖的美梦送给了每一个睡不好觉的小伙伴"
+story_title = "小刺猬嘟嘟的月光灯笼"
+story_desc = "在森林深处的枫叶林里，小刺猬嘟嘟捡到了一个月光灯笼，每天夜晚用温柔的光送每一位森林伙伴安全回家"
 url = "https://zhengcls.github.io/bedtime-story/"
 
 card = {
